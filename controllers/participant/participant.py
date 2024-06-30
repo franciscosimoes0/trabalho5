60ad6b3885c7cd122a52105ad9e566ea991b0c1d
franciscosimoes0 <98843991+franciscosimoes0@users.noreply.github.com>
2024-07-01 00:16:07 +0100

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -13,6 +13,8 @@ maxSpeed = 8.72
 pitchMotor = robot.getDevice("body pitch motor")
 pitchMotor.setPosition(float('inf'))
 pitchMotor.setVelocity(0.0)
+gyro = robot.getDevice("body gyro")
+gyro.enable(timestep)
 
 # This is the time interval between direction switches.
 # The robot will start by going forward and will go backward after
@@ -23,15 +25,24 @@ timeInterval = 1.5
 pitchMotor.setVelocity(maxSpeed)
 forward = True
 lastTime = 0
-
+values_old=gyro.getValues()
 while robot.step(timestep) != -1:
     now = robot.getTime()
+    #print(values)
     # We check if enough time has elapsed.
     if now - lastTime > timeInterval:
         # If yes, then we switch directions.
-        if forward:
+        values_new=gyro.getValues()
+        delta =values_new[1]-values_old[1]
+        #print(delta) 
+        if delta>0.1:
+            #aceleracao positiva
+            pitchMotor.setVelocity(maxSpeed)
+        elif delta<-0.1:
+            #aceleracao negativa
             pitchMotor.setVelocity(-maxSpeed)
         else:
-            pitchMotor.setVelocity(maxSpeed)
-        forward = not forward
+            #aceleracao proxima de zero
+            pitchMotor.setVelocity(0.0)
         lastTime = now
+        values_old=gyro.getValues()
